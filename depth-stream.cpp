f2ed758f6af5ba8f3f5ca5b6f80ce81a9046882a
Zach Leskovec <33209886+zlesk64@users.noreply.github.com>
2018-11-09 08:23:23 -0800

    updated depth-stream

--- depth-stream.cpp
+++ depth-stream.cpp
@@ -1,7 +1,7 @@
 #include <librealsense2/rs.hpp>
 #include <opencv2/opencv.hpp>
 
-//using namespace cv;
+using namespace cv;
 //using namespace std;
 
 int main () 
@@ -15,8 +15,6 @@ int main ()
     //Start Stream
     pipe.start(cfg);
     rs2::frameset frames;
-    using namespace cv;
-//    const auto window_name = "Display Depth";
     //Create Windows
     namedWindow("Depth", WINDOW_AUTOSIZE);
     namedWindow("Color", WINDOW_AUTOSIZE);
@@ -25,15 +23,11 @@ int main ()
     while (waitKey(1) < 0 && cvGetWindowHandle("Color") && cvGetWindowHandle("Depth")) {
         rs2::frameset data = pipe.wait_for_frames();
         rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
-        rs2::frame color_frame = data.get_color_frame();/*.apply_filter(color_map);*/
-
-       // const int width = depth.as<rs2::video_frame>().get_width();
-       // const int height = depth.as<rs2::video_frame>().get_height();
-
+        rs2::frame color_frame = data.get_color_frame();
         Mat image(Size(640, 480), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
         Mat color(Size(640, 480), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
-       imshow("Color", color);
-       imshow("Depth", image);
+        imshow("Color", color);
+        imshow("Depth", image);
     }
 
     return EXIT_SUCCESS;
